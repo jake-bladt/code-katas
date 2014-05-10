8c4b36e4d525093f6a82ffecf888219b8c75d5ec
Jake Bladt <jake@jakebladt.com>
2014-05-10 10:45:35 -0400

    generate sample app

---
+++ 007-hartl_variations/sample_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '2415ce9545240157dfa9ce0028bc72b0fde9b27ec1eb9b7c6b54b930f55ab4b67cf8f73a989b75687cead32da8242586d0a38fc10b597005ab4f03ffc812c9f4'
