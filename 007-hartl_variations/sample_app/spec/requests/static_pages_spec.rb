dc9c6227cbcbece1f9ceb0407ff3950a1a0a6e7c
Jake Bladt <jake@jakebladt.com>
2014-05-10 12:04:04 -0400

    DRY spec

--- 007-hartl_variations/sample_app/spec/requests/static_pages_spec.rb
+++ 007-hartl_variations/sample_app/spec/requests/static_pages_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  let(:title_prefix) { "Ruby on Rails Tutorial Sample App |" }
 
   describe "Home page" do
     it "should have the content 'Sample App'" do
@@ -10,7 +11,7 @@ describe "Static pages" do
 
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("#{title_prefix} Home")
     end
   end
 
@@ -22,7 +23,7 @@ describe "Static pages" do
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{title_prefix} Help")
     end
   end
 
@@ -34,7 +35,7 @@ describe "Static pages" do
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title("#{title_prefix} About Us")
     end
   end
 
@@ -51,7 +52,7 @@ describe "Static pages" do
 
     it "should have the title 'Contact Us'" do
       visit '/static_pages/contact'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact Us")
+      expect(page).to have_title("#{title_prefix} Contact Us")
     end
   end
 
