8133cc3eb24678efc0901382ec5ec7ac97132cb2
Jake Bladt <jake@jakebladt.com>
2014-05-12 13:05:20 -0400

    compact specs

--- 007-hartl_variations/sample_app/spec/requests/static_pages_spec.rb
+++ 007-hartl_variations/sample_app/spec/requests/static_pages_spec.rb
@@ -2,61 +2,31 @@ require 'spec_helper'
 
 describe "Static pages" do
   let(:title_prefix) { "Ruby on Rails Tutorial Sample App" }
+  subject { page }
 
   describe "Home page" do
     before { visit root_path }
-    
-    it "should have the content 'Sample App'" do
-      expect(page).to have_content('Sample App')
-    end
-
-    it "should have the standard title" do
-      expect(page).to have_title("#{title_prefix}")
-    end
-
-    it "should not have a custom page title" do
-      expect(page).not_to have_title('| Home')
-    end
+    it { should have_content('Sample App') }
+    it { should have_title("#{title_prefix}") }
+    it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
     before { visit help_path }
-    
-    it "should have the content 'Help'" do
-      expect(page).to have_content('Help')
-    end
-
-    it "should have the title 'Help'" do
-      expect(page).to have_title("#{title_prefix} | Help")
-    end
+    it { should have_content('Help') }
+    it { should have_title("#{title_prefix} | Help") }
   end
 
   describe "About page" do
     before { visit about_path }
-
-    it "should have the content 'About Us'" do
-      expect(page).to have_content('About Us')
-    end
-
-    it "should have the title 'About Us'" do
-      expect(page).to have_title("#{title_prefix} | About Us")
-    end
+    it { should have_content('About Us') }
+    it { should have_title("#{title_prefix} | About Us") }
   end
 
   describe "Contact page" do
     before { visit contact_path }
-
-    it "should have the content 'Contact Us'" do
-      expect(page).to have_content('Contact Us')
-    end
-
-    it "should include my email address" do
-      expect(page).to have_content('jake@jakebladt.com')
-    end
-
-    it "should have the title 'Contact Us'" do
-      expect(page).to have_title("#{title_prefix} | Contact Us")
-    end
+    it { should have_content('Contact Us') }
+    it { should have_content('jake@jakebladt.com') }
+    it { should have_title("#{title_prefix} | Contact Us") }
   end
-
 end
