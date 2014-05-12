24d0e3a8d46a27704955170e735e815b851bec16
Jake Bladt <jake@jakebladt.com>
2014-05-12 15:51:20 -0400

    use have_selector

--- 007-hartl_variations/sample_app/spec/requests/static_pages_spec.rb
+++ 007-hartl_variations/sample_app/spec/requests/static_pages_spec.rb
@@ -6,26 +6,26 @@ describe "Static pages" do
 
   describe "Home page" do
     before { visit root_path }
-    it { should have_content('Sample App') }
+    it { should have_selector('h1','Sample App') }
     it { should have_title("#{title_prefix}") }
     it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
     before { visit help_path }
-    it { should have_content('Help') }
+    it { should have_selector('h1','Help') }
     it { should have_title("#{title_prefix} | Help") }
   end
 
   describe "About page" do
     before { visit about_path }
-    it { should have_content('About Us') }
+    it { should have_selector('h1','About Us') }
     it { should have_title("#{title_prefix} | About Us") }
   end
 
   describe "Contact page" do
     before { visit contact_path }
-    it { should have_content('Contact Us') }
+    it { should have_selector('h1','Contact Us') }
     it { should have_content('jake@jakebladt.com') }
     it { should have_title("#{title_prefix} | Contact Us") }
   end
