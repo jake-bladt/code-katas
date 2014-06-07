46e30edc8e0b78595a3edb3733ad37280d269236
Jake Bladt <jake@jakebladt.com>
2014-06-06 23:21:07 -0400

    guess

---
+++ 008-clisp/001-guess/guess.lisp
@@ -0,0 +1,16 @@
+(defparameter *small* 1)
+(defparamter  *big* 100)
+
+(defun guess-my-number ()
+  (ash (+ *small* *big*) -1)
+)
+
+(defun smaller ()
+  (setf *big* (1- (guess-my-number)))
+  (guess-my-number)
+)
+
+(defun bigger ()
+  (setf *small* (1+ (guess-my-number)))
+  (guess-my-number)
+)
