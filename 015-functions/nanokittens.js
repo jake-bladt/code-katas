0a505a50acc2783ab259b3e2dc1886d55c3ca578
Jake Bladt <jake@jakebladt.com>
2014-12-05 23:48:24 -0500

    use factories

--- 015-functions/nanokittens.js
+++ 015-functions/nanokittens.js
@@ -19,6 +19,32 @@ var theNanoKittens = {
 	}
 };
 
-var kittenFactory = theNanoKittens.getFactory('cat', 3);
-var mkFactory = theNanoKittens.getFactory('cat', 4);
-var puppyFactory = theNanoKittens.getFactory('dog', 2);
+var makeKitten    = theNanoKittens.getFactory('cat', 3);
+var makeMotherCat = theNanoKittens.getFactory('cat', 4);
+var makePuppy     = theNanoKittens.getFactory('dog', 2);
+
+// Create some critters that can become kittens.
+var milo   = theNanoKittens.getCritter('Milo');
+var rufus  = theNanoKittens.getCritter('Rufus');
+var runt   = theNanoKittens.getCritter('Runt');
+
+// Turn them into kittens
+makeKitten(milo);
+makeKitten(rufus);
+makeKitten(runt);
+
+// Examine them
+console.log(milo.toString());
+console.log(rufus.toString());
+console.log(runt.toString());
+
+// Create a mother cat
+var tom = makeMotherCat(theNanoKittens.getCritter('Tom Frost'));
+console.log(tom.toString());
+
+// Give them a couple of puppies to play with
+var spike = theNanoKittens.getCritter('Spike');
+var fido  = theNanoKittens.getCritter('Fido');
+
+console.log(spike.toString());
+console.log(fido.toString());
