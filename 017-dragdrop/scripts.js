4933d534864b8a7b043eb9b8f870cec1aaba7a21
Jake <jake@jakebladt.com>
2015-02-24 17:42:44 -0500

    -chyron

--- 017-dragdrop/scripts.js
+++ 017-dragdrop/scripts.js
@@ -19,8 +19,7 @@ $(document).ready(function() {
       var $dropped = ui.draggable;
       var newVal = Number($dropped.attr('data-numeric-value'));
 
-      var $chyron = $(this).siblings('.score-chyron');
-      var $scoreElement = $chyron.find('.score-number');
+      var $scoreElement = $(this).siblings('.score-chyron').find('.score-number');
       var oldScore = Number($scoreElement.text());
       var newScore = newVal + oldScore;
       $scoreElement.text(newScore);
