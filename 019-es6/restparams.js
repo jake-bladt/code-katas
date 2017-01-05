266c08942ed0d23169f7645690216f19d3fa9ec8
Jake Bladt <jake@jakebladt.com>
2017-01-05 07:22:59 -0500

    +rest params

--- 019-es6/restparams.js
+++ 019-es6/restparams.js
@@ -1,7 +1,13 @@
 const app = {
-  process: (settings, ...subjects) {
-
+  process: (operations, ...subjects) => {
+    const ret = operations.map((o) => o.title);
+    return ret;
   }
 }
+
+const ops = [
+  { title: 'add kitten love', operation: (s) => `${s} loves kittens.` },
+  { title: 'apply kitten love', operation: (s) => `{s} is loved by kittens.` }
+];
 
 
