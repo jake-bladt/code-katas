984c0c61b93b2b0d4b5301bbf58bffb67a28758e
Jake Bladt <jake@jakebladt.com>
2017-01-05 08:05:11 -0500

    +$

--- 019-es6/restparams.js
+++ 019-es6/restparams.js
@@ -8,7 +8,7 @@ const app = {
 
 const ops = [
   { title: 'add kitten love', operation: (s) => `${s} loves kittens.` },
-  { title: 'apply kitten love', operation: (s) => `{s} is loved by kittens.` }
+  { title: 'apply kitten love', operation: (s) => `${s} is loved by kittens.` }
 ];
 
 var artifacts = app.process(ops, 'Jake', 'Tom', 'Tomasz');
