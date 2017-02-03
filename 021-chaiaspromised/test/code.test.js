5943c97ac4848780df3182df5ae98707b778d188
Jake Bladt <jake@jakebladt.com>
2017-02-03 08:16:36 -0500

    +chai as promised

---
+++ 021-chaiaspromised/test/code.test.js
@@ -0,0 +1,8 @@
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
+const fs = require('fs')
+
+chai.use(chaiAsPromised)
+const assert = chai.assert
+const expect = chai.expect
+const should = chai.should
