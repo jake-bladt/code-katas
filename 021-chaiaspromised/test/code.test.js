630f2e7e2cae5e1512531ac5654e34257e2987e5
Jake Bladt <jake@jakebladt.com>
2017-02-03 08:33:52 -0500

    +basic tests

--- 021-chaiaspromised/test/code.test.js
+++ 021-chaiaspromised/test/code.test.js
@@ -6,3 +6,14 @@ chai.use(chaiAsPromised)
 const assert = chai.assert
 const expect = chai.expect
 const should = chai.should
+
+describe('Standard Test Responses', () => {
+  it('should pass this test', function() {
+    return Promise.resolve('this promise will always be resolved');
+  })
+
+  it('should fail this test', function() {
+    return Promise.reject('this promise will always be rejected');
+  })
+
+})
